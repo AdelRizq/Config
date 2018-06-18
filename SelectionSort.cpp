11d7574abf477d3abb8911caf5c38fbf4d83ea51
AdelRizq <adel.m.rizq@gmail.com>
2018-06-18 09:42:50 +0200

    add code

---
+++ SelectionSort.cpp
@@ -0,0 +1,29 @@
+# This Function takes an array as input and sorts it using Selection Sort
+# Return the address of the first element of the sorted array
+# Author: Adel Rizq
+# Date: 6/18/2018
+
+void swap (int &i , int &j)
+{
+	i = i+j;
+	j = i-j;
+	i = i-j;
+} 
+
+
+int* selection(int a[], int size)
+{
+	int min, i, j, k = 0;
+	for (i = 0; i< size-1; i++)
+	{
+		mi = a[i];
+		for (j = i; j <= size-1; j++)
+			if (a[j]<min)
+			{
+				min = a[j];
+				k = j;
+			}
+		swap(a[k], a[i]);
+	}
+	return &a[0];
+}
