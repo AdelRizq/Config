abf85f97b426d90041d4139661d2197869087acd
AdelRizq <40351413+AdelRizq@users.noreply.github.com>
2018-06-24 20:29:26 +0200

    initial

---
+++ Stack.cpp
@@ -0,0 +1,26 @@
+//Stack by array
+//Author : Adel Rizq
+//Date : 6/24/2018
+
+
+struct stack{
+	int arr[40];
+	int top = -1;
+	void push(int val){
+		if (top == 99) return;
+		top++;
+		arr[top] = val;
+	}
+	void pop(){
+		if (isempty()) return; 
+		top--;
+	}
+	int top_val(){
+		if (isempty()) return 0;
+		else return arr[top];
+	}
+	bool isempty(){
+		if (top == -1) return true;
+		else return false;
+	}
+};
